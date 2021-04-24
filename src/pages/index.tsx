aee190db7562718448aa0957f6671e809b0f314f
Aloysio Augusto Rabello de Carvalho <aloysio.rabello@gmail.com>
2021-04-24 20:08:12 -0300

    add axios

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,39 @@
-export default function Home() {
-  return <h1>Index</h1>
+import { GetStaticProps } from 'next'
+import { api } from '../services/api'
+
+type Episodes = {
+  id: string
+  title: string
+  members: string
+  published_at: string
+}
+
+type HomeProps = {
+  episodes: Episodes[]
+}
+
+export default function Home(props: HomeProps) {
+  return (
+    <div>
+      <h1>Index server data:</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc',
+    },
+  })
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  }
 }

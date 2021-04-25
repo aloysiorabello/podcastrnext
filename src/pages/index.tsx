edab6949cdf0c271d50e0e187659cd437f406ec8
Aloysio Augusto Rabello de Carvalho <aloysio.rabello@gmail.com>
2021-04-25 14:33:39 -0300

    add playerContext

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,13 @@
+import { useContext } from 'react'
 import { GetStaticProps } from 'next'
 import Image from 'next/image'
 import Link from 'next/link'
 import { format, parseISO } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
+
 import { api } from '../services/api'
 import { converDurationToTimeString } from '../utils/convertDurationToTimeString'
+import { PlayerContext } from '../contexts/PlayerContext'
 
 import styles from './home.module.scss'
 
@@ -25,6 +28,8 @@ type HomeProps = {
 }
 
 export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
+  const { play } = useContext(PlayerContext)
+
   return (
     <div className={styles.homepage}>
       <section className={styles.latestEpisodes}>
@@ -51,7 +56,7 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
                   <span>{episode.durationAsString}</span>
                 </div>
 
-                <button type='button'>
+                <button type='button' onClick={() => play(episode)}>
                   <img src='/play-green.svg' alt='Tocar episodio' />
                 </button>
               </li>
@@ -125,9 +130,13 @@ export const getStaticProps: GetStaticProps = async () => {
       title: episode.title,
       thumbnail: episode.thumbnail,
       members: episode.members,
-      published_at: format(parseISO(episode.published_at), 'd MMM yy', {locale: ptBR}),
+      published_at: format(parseISO(episode.published_at), 'd MMM yy', {
+        locale: ptBR,
+      }),
       duration: Number(episode.file.duration),
-      durationAsString: converDurationToTimeString(Number(episode.file.duration)),
+      durationAsString: converDurationToTimeString(
+        Number(episode.file.duration)
+      ),
       url: episode.file.url,
     }
   })
@@ -144,4 +153,4 @@ export const getStaticProps: GetStaticProps = async () => {
   }
 }
 
-//1:01:30
+//42:50

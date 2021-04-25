7846ea675076d5f3d3337d2279e88d416dcce54b
Aloysio Augusto Rabello de Carvalho <aloysio.rabello@gmail.com>
2021-04-25 13:43:03 -0300

    add ssg for latest episodes

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -2,8 +2,9 @@ import { format, parseISO } from 'date-fns'
 import Image from 'next/image'
 import Link from 'next/link'
 import { ptBR } from 'date-fns/locale'
-import { GetStaticPaths, GetStaticProps } from 'next'
 import { useRouter } from 'next/router'
+import next, { GetStaticPaths, GetStaticProps } from 'next'
+
 import { api } from '../../services/api'
 import { converDurationToTimeString } from '../../utils/convertDurationToTimeString'
 
@@ -61,8 +62,24 @@ export default function Episode({ episode }: EpisodeProps) {
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 2,
+      _sort: 'published_at',
+      _order: 'desc',
+    },
+  })
+
+  const paths = data.map((episode) => {
+    return {
+      params: {
+        slug: episode.id,
+      },
+    }
+  })
+
   return {
-    paths: [],
+    paths,
     fallback: 'blocking',
   }
 }
